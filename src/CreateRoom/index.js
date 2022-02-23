62022b00dbea0f19d64a8ae4b0f66459d061c06b
Jatin Arora <arorajatin003@gmail.com>
2022-02-23 19:05:57 +0530

    Added create room

--- src/CreateRoom/index.js
+++ src/CreateRoom/index.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react'
+// import {AddCircleOutlineIcon} from '@material-ui/icons';
+// import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import "./style.css";
 
 const CreateRoom = ({view})=>{
     const [name,setName] = useState('');
+    const [mode, setMode] = useState(0);
+    const [email, setEmail] = useState('');
+    const [members, setMembers] = useState([]);
+
+    const addMember = (e)=>{
+        if(e.key==='Enter'){
+            setMembers([...members, email ]);
+            setEmail('');
+            console.log(members);
+        }
+    }
+    const deleteMember = (idx)=>{
+        members.filter((data,i)=>i!==idx);
+    }
     return (
         <div className='createRoom'>
            <div className='createRoom_header'>
@@ -18,6 +34,59 @@ const CreateRoom = ({view})=>{
                         placeholder="Enter the chat name"
                     />
                </div>
+               <div className='createRoom_input_wrapper'>
+                    <label>Mode</label>
+                    <br />
+                    <div className="createRoom_input_bigRadio_wrapper">
+                        <div 
+                            className={`createRoom_input_bigRadio ${mode===0 && 'createRoom_input_bigRadio_clicked'}`}
+                            onClick={()=>setMode(0)}
+                        >
+                            Public
+                        </div>
+                        <div 
+                            className={`createRoom_input_bigRadio ${mode===1 && 'createRoom_input_bigRadio_clicked'}`}
+                            onClick={()=>setMode(1)}
+                        >
+                            Sami-public
+                        </div>
+                        <div 
+                            className={`createRoom_input_bigRadio ${mode===2 && 'createRoom_input_bigRadio_clicked'}`}
+                            onClick={()=>setMode(2)}
+                        >
+                            Private
+                        </div>
+                    </div>
+               </div>
+               {mode>0?
+                    <div className='createRoom_input_wrapper'>
+                        <label>Add members</label>
+                        <br />
+                        <input 
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            placeholder="Enter user email and press enter to add"
+                            onKeyPress={addMember}
+                        />
+                        { members.length>0 && 
+                            <div className="membersList">
+                                {
+                                    members.map((val,idx)=>{
+                                        return(
+                                            <div className="members_wrapper">
+                                                {val}
+                                                <div className="deleteMember" onClick={()=>deleteMember(idx)}>
+                                                    +
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        }
+                    </div>
+                :""
+               }
            </div>
         </div>
     )
